2b2374bfe51423c333889a1dc31eaf8cb8648d37
Гордей <Gordei@MacBook-Pro-Gordej.local>
2015-07-22 14:30:35 +0300

    Add hash, size to sprite

--- webpack-plugin.js
+++ webpack-plugin.js
@@ -2,23 +2,29 @@
 'use strict'
 
 var fs = require('fs');
+var crypto = require('crypto');
 
 var WebpackPlugin = function (svg1, svg2) {
     this.svg1 = svg1;
     this.svg2 = svg2;
 };
 
-WebpackPlugin.prototype.apply = function (compiler) {
+WebpackPlugin.prototype.hash = function (buffer) {
+  return crypto.createHash('md5').update(buffer).digest('hex');
+};
 
+WebpackPlugin.prototype.apply = function (compiler) {
+  var self = this;
   var oneSvg = fs.readFileSync(this.svg1, 'utf-8');
   var secondSvg = fs.readFileSync(this.svg2, 'utf-8');
 
   var sprite = oneSvg + secondSvg;
 
   compiler.plugin('emit', function(compilation, callback) {
-    compilation.assets['svg/my-sprite.svg'] = {
-      source: function() { return sprite },
-      size: function() { return 5; }
+    
+    compilation.assets['svg/my-sprite.' + self.hash(sprite) + '.svg'] = {
+      source: function() { return new Buffer(sprite) },
+      size: function() { return Buffer.byteLength(this.source(), 'utf8'); }
     }
     callback();
   });
